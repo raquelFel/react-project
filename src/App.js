5172cf3934efeccfb17f34c4440fbb19ec55f046
raquelFel <55757812+raquelFel@users.noreply.github.com>
2019-11-26 11:16:34 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello App!</h1>
-      </header>
-      <footer>
-        {" "}
-        This project was coded by{" "}
-        <a href="https://raquelfelgueiras.com/">Raquel Felgueiras </a>and is{" "}
-        {""}
-        <a href="https://github.com/raquelFel/react-project" target="_blank">
-          Open-source on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          {" "}
+          This project was coded by{" "}
+          <a href="https://raquelfelgueiras.com/">Raquel Felgueiras </a>and is{" "}
+          {""}
+          <a href="https://github.com/raquelFel/react-project" target="_blank">
+            Open-source on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
