e68cfd8f81281a18eddf5a974b2f707906d85ba1
raquelFel <55757812+raquelFel@users.noreply.github.com>
2019-11-26 10:58:11 +0000

    Inserted github repo URL

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
       <header className="App-header">
         <h1>Hello App!</h1>
       </header>
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a href="https://raquelfelgueiras.com/">Raquel Felgueiras </a>and is{" "}
+        {""}
+        <a href="https://github.com/raquelFel/react-project" target="_blank">
+          Open-source on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
